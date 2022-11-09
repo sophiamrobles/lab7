68aa877244e127da09db7c078f368fcfa0123986
sophiamrobles <srobles2@hawk.iit.edu>
2022-11-08 21:34:51 -0600

    update script.js

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@ function init(){
     // create alert box containing text field
     function showName(){
         // get input information
-
+        var input=getElementById('entryinput').value;
+        alert("Sophia Robles:"+input);
         // get output information 
-
+        var ouput=getElementById('textoutput');
+        output.innerHTML=input;
     }
     // create button
-
+    var button=document.getElementById('entrybutton');
+    button.addEventListener('click',showName);
 }
     window.addEventListener('load', init);
